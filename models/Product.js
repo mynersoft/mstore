183ee6b28e6f9e58bf78225b758f150b9a2757ec
Myner Soft <mynersoft@gmail.com>
2025-10-10 17:45:47 +0600

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, models, model } from "mongoose";
 
 const ProductSchema = new mongoose.Schema({
 	name: { type: String, required: true },
