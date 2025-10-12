cc200e247fa03dfe55822b6a2a74c9b882ca7351
Myner Soft <mynersoft@gmail.com>
2025-10-12 07:45:05 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,13 @@
 import "./globals.css";
 import ReduxProvider from "@/app/providers/ReduxProvider";
+import Header from "@/components/Header"
 
 export const metadata = { title: "Inventory Dashboard" };
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+<Header/>
 			<body>
 				<ReduxProvider>{children}</ReduxProvider>
 			</body>
