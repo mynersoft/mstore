9355a177c78c1dee7f4ff53c32a637fe3f3ecc7b
Myner Soft <mynersoft@gmail.com>
2025-10-12 07:25:15 +0600

    Update store.js

--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./productSlice";
+import duesReducer from "./duesSlice"
 
 export const store = configureStore({
-	reducer: { products: productReducer },
+	reducer: { products: productReducer,
+dues:duesReducer },
 });
