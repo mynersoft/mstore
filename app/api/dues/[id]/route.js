5986d2a708338877670c09ff02e21a2e89d030f8
Myner Soft <mynersoft@gmail.com>
2025-10-12 07:15:24 +0600

    Update route.js

--- app/api/dues/[id]/route.js
+++ app/api/dues/[id]/route.js
@@ -1,18 +1,18 @@
 import { connectDB } from "@/lib/dbConnect";
-import Due from "@/models/Due";
+import CustomerDue from "@/models/Due";
 import { NextResponse } from "next/server";
 
 export async function PUT(req, { params }) {
   await connectDB();
-  const { id } = params;
   const body = await req.json();
-  const updated = await Due.findByIdAndUpdate(id, body, { new: true });
+  const updated = await CustomerDue.findByIdAndUpdate(params.id, body, {
+    new: true,
+  });
   return NextResponse.json(updated);
 }
 
 export async function DELETE(req, { params }) {
   await connectDB();
-  const { id } = params;
-  await Due.findByIdAndDelete(id);
+  await CustomerDue.findByIdAndDelete(params.id);
   return NextResponse.json({ message: "Deleted successfully" });
 }
