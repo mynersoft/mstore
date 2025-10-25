c4276c77fff7f79e0adb50d8b6d9c228cbd232a1
Myner Soft <mynersoft@gmail.com>
2025-10-25 17:28:29 +0600

    Update store.js

--- redux/store.js
+++ redux/store.js
@@ -2,10 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./productSlice";
 import dueReducer from "./dueSlice";
 import serviceReducer from "./serviceSlice";
+import categoryReducer from "./categorySlice";
+import brandReducer from "./brandSlice";
+import subCategoryReducer from "./subCategorySlice";
+
 
 export const store = configureStore({
-	reducer: {
-		products: productReducer,
-		service: serviceReducer,
-		dues:dueReducer },
+  reducer: {
+    products: productReducer,
+    categories: categoryReducer,
+    brands: brandReducer,
+service: serviceReducer,
+		dues:dueReducer,
+    subCategories: subCategoryReducer,
+  },
 });
+
+
