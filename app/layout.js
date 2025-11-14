fc4ee402d1ae39176ec41a739ac740628572580e
Myner Soft <mynersoft@gmail.com>
2025-11-14 23:02:55 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
 "use client";
 
-
-
 import "./globals.css";
 import ReduxProvider from "@/app/providers/ReduxProvider";
 import { GlobalInitializer } from "@/components/fetch/GlobalInitializer";
@@ -12,36 +10,39 @@ import { useEffect, useState } from "react";
 import Loader from "@/components/Loader";
 
 export const metadata = {
-	title: "Bismillah Telecom & Servicing",
-	description: "Admin dashboard for managing products and sales",
+  title: "Bismillah Telecom & Servicing",
+  description: "Admin dashboard for managing products and sales",
 };
 
 export default function RootLayout({ children }) {
-
-
-const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // সব কন্টেন্ট লোড হলে loader remove হবে
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 800); // লোডার একটু smooth দেখানোর জন্য
+    }, 800); // smooth delay
 
     return () => clearTimeout(timer);
   }, []);
 
+  return (
+    <html lang="en">
+      <body className="dark bg-gray-900 text-gray-100">
+        {/* Loader */}
+        {loading && <Loader />}
 
+        {!loading && (
+          <>
+            <Header />
 
-	return (
-		<html lang="en">
-			<body className="dark bg-gray-900 text-gray-100">
-				<Header />
-
-				<ReduxProvider>
-					<GlobalInitializer />
-					{children}
-				</ReduxProvider>
-			</body>
-		</html>
-	);
+            <ReduxProvider>
+              <GlobalInitializer />
+              {children}
+            </ReduxProvider>
+          </>
+        )}
+      </body>
+    </html>
+  );
 }
