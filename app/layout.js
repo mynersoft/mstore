82ae98d11659d2e00f99340ca850cbca53ef7f49
Myner Soft <mynersoft@gmail.com>
2025-11-14 22:47:19 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,12 +3,30 @@ import ReduxProvider from "@/app/providers/ReduxProvider";
 import { GlobalInitializer } from "@/components/fetch/GlobalInitializer";
 import Header from "@/components/Header";
 
+
+import Loader from "@/components/Loader";
+
 export const metadata = {
 	title: "Bismillah Telecom & Servicing",
 	description: "Admin dashboard for managing products and sales",
 };
 
 export default function RootLayout({ children }) {
+
+
+const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // সব কন্টেন্ট লোড হলে loader remove হবে
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 800); // লোডার একটু smooth দেখানোর জন্য
+
+    return () => clearTimeout(timer);
+  }, []);
+
+
+
 	return (
 		<html lang="en">
 			<body className="dark bg-gray-900 text-gray-100">
