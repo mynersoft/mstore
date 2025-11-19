cb88526992e14b8eba73c5ba999a3fbb08219d97
Myner Soft <mynersoft@gmail.com>
2025-11-19 23:36:55 +0600

    Update route.js

--- app/api/google-image-search/route.js
+++ app/api/google-image-search/route.js
@@ -2,7 +2,7 @@ export async function POST(req) {
   try {
     const { query } = await req.json();
 
-    const GOOGLE_API_KEY = AIzaSyAt78sBTwCqXnU3WERRTPgN25GVZRgYiio;
+    const GOOGLE_API_KEY = "AIzaSyAt78sBTwCqXnU3WERRTPgN25GVZRgYiio";
     const CX = "8321052658f004181";
 
     const url = `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${CX}&searchType=image&q=${encodeURIComponent(
