f2b900a1a8a650aa91d42d7d7be6d758ff20381c
Myner Soft <mynersoft@gmail.com>
2025-11-19 23:29:28 +0600

    Update route.js

--- app/api/google-image-search/route.js
+++ app/api/google-image-search/route.js
@@ -2,13 +2,20 @@ export async function POST(req) {
   try {
     const { query } = await req.json();
 
-    const url = `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CX}&searchType=image&q=${query}`;
+    const GOOGLE_API_KEY = AIzaSyAt78sBTwCqXnU3WERRTPgN25GVZRgYiio;
+    const CX = "8321052658f004181";
+
+    const url = `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${CX}&searchType=image&q=${encodeURIComponent(
+      query
+    )}`;
 
     const res = await fetch(url);
     const data = await res.json();
 
-    return Response.json({ images: data.items });
-  } catch (err) {
-    return Response.json({ error: err.message }, { status: 500 });
+console.lof(data.items);
+
+    return Response.json({ images: data.items || [] });
+  } catch (error) {
+    return Response.json({ error: error.message }, { status: 500 });
   }
 }
