35b71b0d482d9b838df6277381bcac32dd6234d6
Myner Soft <mynersoft@gmail.com>
2025-11-22 15:09:22 +0600

    Update page.jsx

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchProducts } from "@/redux/productsSlice";
+import { fetchProducts } from "@/redux/productSlice";
 import ShopFilters from "@/components/ShopFillters";
 import ShopList from "@/components/ShopLists";
 
