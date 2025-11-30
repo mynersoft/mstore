6647e3d2cc3209d31ac5701fc6296649cfb1067f
Myner Soft <mynersoft@gmail.com>
2025-11-30 18:17:57 +0600

    Update Bill.js

--- models/Bill.js
+++ models/Bill.js
@@ -4,6 +4,9 @@ const BillSchema = new Schema(
   {
     name: { type: String, required: true },
     amount: { type: Number, required: true },
+    month: { type: String, required: true }, // YYYY-MM
+    status: { type: String, default: "paid" }, // "paid" | "due"
+    note: { type: String },
   },
   { timestamps: true }
 );
