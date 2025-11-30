bd65ee23a8412258f99b3d04f710da601fba3bd0
Myner Soft <mynersoft@gmail.com>
2025-11-30 09:37:22 +0600

    Update Invest.js

--- models/Invest.js
+++ models/Invest.js
@@ -5,7 +5,7 @@ const InvestSchema = new mongoose.Schema(
     name: { type: String, required: true },
     investType: {
       type: String,
-      enum: ["malamal", "tools", " others"],
+      enum: ["malamal", "tools", "others"],
       required: true,
     },
     amount: { type: Number, required: true },
