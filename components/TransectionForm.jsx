f599d10539f786c3b4569a85a32f2d8a713ed5af
Myner Soft <mynersoft@gmail.com>
2025-12-11 00:05:07 +0600

    Update TransectionForm.jsx

--- components/TransectionForm.jsx
+++ components/TransectionForm.jsx
@@ -2,7 +2,7 @@
 "use client";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { addTransaction  } from " @/redux/transactionSlice";
+import { addTransaction  } from "@/redux/transactionSlice";
 
 export default function AddTransactionForm() {
   const dispatch = useDispatch();
