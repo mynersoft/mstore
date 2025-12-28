897b4846ecf248932fe95654fa545da3115b7df7
Myner Soft <mynersoft@gmail.com>
2025-12-28 07:25:51 +0600

    Create aybay/route.js

---
+++ app/api/aybay/route.js
@@ -0,0 +1,24 @@
+import connectDB from "@/lib/connectDB";
+import AyBay from "@/models/AyBay";
+
+export async function GET() {
+  await connectDB();
+  const data = await AyBay.find().sort({ createdAt: -1 });
+
+  return Response.json({
+    success: true,
+    data,
+  });
+}
+
+export async function POST(req) {
+  await connectDB();
+  const body = await req.json();
+
+  const newAyBay = await AyBay.create(body);
+
+  return Response.json({
+    success: true,
+    data: newAyBay,
+  });
+}
