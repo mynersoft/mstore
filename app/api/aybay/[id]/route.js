6c101e6944acd3c61a4efdd3fec5721788956eab
Myner Soft <mynersoft@gmail.com>
2025-12-28 08:16:39 +0600

    Update route.js

--- app/api/aybay/[id]/route.js
+++ app/api/aybay/[id]/route.js
@@ -32,3 +32,38 @@ export async function PUT(req, { params }) {
     data: updatedAyBay,
   });
 }
+
+
+
+
+import connectDB from "@/lib/connectDB";
+import AyBay from "@/models/AyBay";
+
+// DELETE by ID
+export async function DELETE(req, { params }) {
+  await connectDB();
+
+  const { id } = params;
+
+  if (!id) {
+    return Response.json(
+      { success: false, message: "ID প্রয়োজন" },
+      { status: 400 }
+    );
+  }
+
+  const deleted = await AyBay.findByIdAndDelete(id);
+
+  if (!deleted) {
+    return Response.json(
+      { success: false, message: "ডাটা পাওয়া যায়নি" },
+      { status: 404 }
+    );
+  }
+
+  return Response.json({
+    success: true,
+    id: deleted._id,
+    message: "ডিলিট সম্পন্ন হয়েছে",
+  });
+}
