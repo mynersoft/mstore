a90a5b5073c530ea2f1972fc0b0771aac5efdb37
Myner Soft <mynersoft@gmail.com>
2025-12-28 07:19:51 +0600

    Create aybaySlice.js

---
+++ redux/aybaySlice.js
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+// fetch আয়-ব্যয়
+export const fetchAyBay = createAsyncThunk(
+  "aybay/fetch",
+  async () => {
+    const res = await fetch("/api/transactions");
+    return res.json();
+  }
+);
+
+// add আয়-ব্যয়
+export const addAyBay = createAsyncThunk(
+  "aybay/add",
+  async (data) => {
+    const res = await fetch("/api/transactions", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    return res.json();
+  }
+);
+
+const aybaySlice = createSlice({
+  name: "aybay",
+  initialState: {
+    list: [],
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAyBay.fulfilled, (state, action) => {
+        state.list = action.payload;
+      })
+      .addCase(addAyBay.fulfilled, (state, action) => {
+        state.list.push(action.payload);
+      });
+  },
+});
+
+export default aybaySlice.reducer;
